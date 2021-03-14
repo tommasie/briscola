8644fbdf9ef1fc82157eb56afcfc5ce17ff703a9
Thomas Collerton <tho.collerton@gmail.com>
2021-03-14 14:20:55 +0100

    Add cardinality to card values

--- src/main/kotlin/it/briscola/game/model/Card.kt
+++ src/main/kotlin/it/briscola/game/model/Card.kt
@@ -4,4 +4,11 @@ data class Card(
     val number: Int,
     val value: Int,
     val suit: Suit,
-)
+) : Comparable<Card> {
+    override fun compareTo(reigning: Card): Int {
+        if(suit != reigning.suit) return -1
+        if(value > reigning.value) return 1
+        if(value == reigning.value && number > reigning.number) return 1
+        return -1
+    }
+}
